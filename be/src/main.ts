96c1eb0afa554629ef13cdf97d82d22250511452
dev boyenn <dev-boyenn@gmail.com>
2023-11-01 16:29:51 +0100

    fighting cors more

--- be/src/main.ts
+++ be/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*', allowedHeaders: '*', methods: '*' });
+  app.enableCors({ origin: '*', allowedHeaders: '*', methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' });
   await app.listen(3000);
 }
 bootstrap();
